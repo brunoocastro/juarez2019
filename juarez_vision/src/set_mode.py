cd6bef4f64000a06fdf9e248cdd810bcb6924777
BrunooCastro <bruno.scastro2012@hotmail.com>
2019-10-22 23:05:15 -0300

    ajustando velocidade

--- juarez_vision/src/set_mode.py
+++ juarez_vision/src/set_mode.py
@@ -3,12 +3,12 @@ from robotis_controller_msgs.srv import SetJointModule
 from std_msgs.msg import String
 
 
-a = SetJointModule()
+params = SetJointModule()
 
-a.joint_name = ['r_hip_yaw', 'r_hip_roll', 'r_hip_pitch', 'r_knee', 'r_ankle_pitch', 'r_ankle_roll', 'l_hip_yaw', 'l_hip_roll', 'l_hip_pitch', 'l_knee', 'l_ankle_pitch', 'l_ankle_roll']
-a.module_name = ['walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module']
+params.joint_name = ['r_hip_yaw', 'r_hip_roll', 'r_hip_pitch', 'r_knee', 'r_ankle_pitch', 'r_ankle_roll', 'l_hip_yaw', 'l_hip_roll', 'l_hip_pitch', 'l_knee', 'l_ankle_pitch', 'l_ankle_roll']
+params.module_name = ['walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module','walking_module']
 
 rospy.Service('robotis/set_present_joint_ctrl_modules', SetJointModule, a)#, self.queue_size)
 
-pub = rospy.Publisher('robotis/walking/command', String, queue_size=1)
+pub = rospy.Publisher('robotis/walking/command', String, queue_size=10)
 pub.publish("walking")
